cdeec8626a8e8dc0722a6e31563f1ff42640e48f
fchipanam <fchipanam@unjbg.edu.pe>
2025-05-31 19:10:26 -0600

    Mejora de la funcion2.cpp: num2 no sera negativo, no sera mayor a num1 y no sera igual a 0

--- funcion2.cpp
+++ funcion2.cpp
@@ -3,7 +3,20 @@ using namespace std;
 void division(int val1, int val2);
 int main (){
     int num1, num2;
-    cin>>num1>>num2;
+    cout<<"            DIVISION  "<<endl;
+    cout<<"           ----------"<<endl;
+    cout<<"Ingrese primer numero: ";
+    cin>>num1;
+    cout<<"Ingrese segundo numero: ";
+    cin>>num2;
+    while(num2 > num1){
+        cout<<"Ingrese un numero que no sea mayor al primero: ";
+        cin>>num2;
+    }
+    while(num2 <= 0){
+        cout<<"Ingrese un numero no negativo y no igual a cero: ";
+        cin>>num2;
+    }
     division(num1, num2);
     return 0;
 }
@@ -21,5 +34,5 @@ void division (int val1, int val2){
             i = i - 1;
         }
     }while(restas >= 0);
-    cout<<"La el cociente de la division es "<<i<<" y su resto es igual a "<<val1<<endl;
+    cout<<"El cociente de la division es "<<i<<" y su resto es igual a "<<val1<<endl;
 }
