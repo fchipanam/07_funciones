c06db07d4a051e1d366a24bd556fe3679de4bfe4
fchipanam <fchipanam@unjbg.edu.pe>
2025-06-01 17:39:38 -0600

    Mejora de la funcion4.cpp: No se permitira numeros iguales o menores a 0

--- funcion4.cpp
+++ funcion4.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int divisores(int);
 int main() {
     int num;
+    cout<<"            SU NUMERO ES PERFECTO?"<<endl;
+    cout<<"           ------------------------"<<endl;
+    cout<<"Ingrese su numero: ";
     cin>>num;
+    while (num <= 0){
+        cout<<"Ingrese un numero que que no sea menor o igual a 0: ";
+        cin>>num;
+    }
     divisores (num);
     if (divisores (num) ==  num){
         cout<<"El numero ingresado es perfecto"<<endl;
